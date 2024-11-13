bed2b517e62fb3d9b7b90788e1e5bcc802cb70e2
Jady le Bars <js.lebars@gmail.com>
2024-11-13 10:41:51 +0100

    started box appearance

--- javascript/inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ javascript/inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,1 +1,15 @@
 console.clear();
+const box = document.querySelector("[data-js=box]");
+const inputColor = document.querySelector("[data-js=input-color]");
+const inputRadius = document.querySelector("[data-js=input-radius]");
+const inputRotation = document.querySelector("[data-js=input-rotation]");
+
+inputColor.addEventListener("input", () => {
+  const hue = inputColor.value;
+  box.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
+});
+
+inputRadius.addEventListener("input", () => {
+  const radiusValue = inputRadius.value;
+  box.style.borderRadius = `${radiusValue}%`;
+});
