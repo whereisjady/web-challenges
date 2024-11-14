fe2e96737b852e947ed493cce89ec7434bd7039b
Jady le Bars <js.lebars@gmail.com>
2024-11-14 07:59:17 +0100

    social media elements created and appended

--- javascript/create-element/js-createelement_social-media-post/js/index.js
+++ javascript/create-element/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,32 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+const body = document.querySelector("body");
+
+// created an article parent
+const newPost = document.createElement("article");
+newPost.classList.add("post");
+
+// // post
+const postContent = document.createElement("p");
+postContent.textContent = "This is a new social media post";
+postContent.classList.add("post__content");
+
+// // footer
+const postFooter = document.createElement("footer");
+postContent.classList.add("post__footer");
+
+// // // span & button
+const postUsername = document.createElement("span");
+postContent.textContent = "@username";
+postContent.classList.add("post__username");
+
+const postButton = document.createElement("button");
+postContent.classList.add("post__button");
+
+footer.appendChild(span);
+footer.appendChild(button);
+post.appendChild(footer);
+newPost.appendChild(post);
+body.appendChild(newPost);
