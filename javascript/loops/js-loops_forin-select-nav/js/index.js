cd39de7f7992a99164e34812adced29a267c3669
Jady le Bars <js.lebars@gmail.com>
2024-11-14 14:07:32 +0100

    part 1 done

---
+++ javascript/loops/js-loops_forin-select-nav/js/index.js
@@ -0,0 +1,49 @@
+console.clear();
+
+const main = document.querySelector('[data-js="main"]');
+
+// Part 1: Creating a Select Input
+
+const languages = {
+  DE: "German",
+  EN: "English",
+  ES: "Spanish",
+  FR: "French",
+  IT: "Italian",
+  NL: "Dutch",
+  PT: "Portuguese",
+  RU: "Russian",
+  ZH: "Chinese",
+};
+
+const select = document.createElement("select");
+select.name = "languages";
+main.append(select);
+
+// --v-- write or modify code below this line --v--
+
+for (const language in languages) {
+  const option = document.createElement("option");
+  option.value = language;
+  option.textContent = languages[language];
+  select.append(option);
+}
+
+// --^-- write or modify code above this line --^--
+
+// Part 2: Creating a Navigation Bar
+
+const nav = {
+  home: { href: "/home", text: "Home" },
+  about: { href: "/about", text: "About" },
+  contact: { href: "/contact", text: "Contact" },
+};
+
+const navElement = document.createElement("nav");
+const ul = document.createElement("ul");
+main.append(navElement);
+navElement.append(ul);
+
+// --v-- write or modify code below this line --v--
+
+// --^-- write or modify code above this line --^--
