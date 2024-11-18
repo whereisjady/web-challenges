5fdd43e7bc1c2aa351fe9eba396250bf92b05e87
Jady le Bars <js.lebars@gmail.com>
2024-11-18 16:15:22 +0100

    didn't save all files yet

--- javascript/structure/js-structure_shapes/components/Square/Square.js
+++ javascript/structure/js-structure_shapes/components/Square/Square.js
@@ -1,3 +1,5 @@
+import { getRandomColor } from "../../utils/randomColor.js";
+
 export const square = document.createElement("div");
 square.classList.add("square");
 square.addEventListener("click", () => {
