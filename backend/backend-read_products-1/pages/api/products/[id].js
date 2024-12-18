dc0b1329fe13b96836c6f58990908a10dc3735db
Jady le Bars <js.lebars@gmail.com>
2024-12-18 16:58:34 +0100

    read products done

---
+++ backend/backend-read_products-1/pages/api/products/[id].js
@@ -0,0 +1,12 @@
+import dbConnect from "@/db/connect";
+import Product from "@/db/models/Product";
+
+export default async function handler(request, response) {
+  await dbConnect();
+  const { id } = request.query;
+
+  if (request.method === "GET") {
+    const products = await Product.findById(id);
+    response.status(200).json(products);
+  }
+}
